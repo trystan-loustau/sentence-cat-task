b7a4fb28fb4cb2feccdf3b1c5a768b9786ad6763
trystan-loustau <loustau@bc.edu>
2025-10-08 14:18:44 -0400

    Update experiment.js

--- experiment_code/experiment.js
+++ experiment_code/experiment.js
@@ -2,50 +2,39 @@
 
 // initialize jsPsych
 const jsPsych = initJsPsych({
-    on_finish: function() {
-        jsPsych.data.displayData()
-    }
-})
+  on_finish: function () {
+    jsPsych.data.displayData();
+  }
+});
 
-// Define your trials below, using stimuli that you have already defined. You do not
-// need to import anything because these files will be combine into one big file (index.html)
-// when you run the experiment
-
+// Instructions trial
 const coolInstructions = {
-    type: jsPsychInstructions, // this is the type of trial (plug-in), which determines what jsPsych does
-    pages: function() { // Instructions, like here, just shows HTML!
-        let pages = []
+  type: jsPsychInstructions,
+  pages: function () {
+    let pages = [];
+    pages.push(introduction_page); // defined in stimuli.js
+    return pages;
+  },
+  allow_keys: false,
+  show_clickable_nav: true,
+  allow_backward: true,
+  show_page_number: true,
+  data: {
+    trial_id: "cool_instructions",
+    data_of_interest_name: "wow! i'm some data!"
+  }
+};
 
-        pages.push(
-            introduction_page, // defined in stimuli.js
-        )
-
-        return pages
-    },
-    allow_keys: false, // for each kind of trial (plug-in), there are different parameters you can set
-    show_clickable_nav: true,
-    allow_backward: true, // for instance, this will show the "previous" button (tho there is no prev page)
-    show_page_number: true,
-    data: {
-        trial_id: "cool_instructions", // add any metadata you want to collect here
-        data_of_interest_name: "wow! i'm some data!"
-    }
-}
-
-// POLITICAL CHARACTERIZATIONS TRIAL (keyboard + top prompt, using timeline_variables)
+// POLITICAL CHARACTERIZATIONS TRIAL (keyboard response, prompt fixed at top)
 const politicalCharacterizationProcedure = {
   timeline: [{
     type: jsPsychHtmlKeyboardResponse,
-
-    // Render prompt + sentence + fixed left/right key hints (as you have)
     stimulus: function () {
       const sentence = jsPsych.timelineVariable('sentence');
       return `
         <div class="exp-wrap">
           <div class="prompt-top">Is the following statement <b>True</b> or <b>False</b>?</div>
-
           <div class="stimulus-centered">${sentence}</div>
-
           <div class="key-reminder">
             <div class="key-col left">
               <div class="key-label">False</div>
@@ -59,51 +48,18 @@ const politicalCharacterizationProcedure = {
         </div>
       `;
     },
-
-    // IMPORTANT: let us handle the key ourselves
-    choices: jsPsych.NO_KEYS,
-
-    // Data you want to store regardless
-    data: function () {
-      return { stimulus: jsPsych.timelineVariable('sentence') };
-    },
-
-    // Add a keyboard listener; highlight and delay before advancing
-    on_load: function () {
-      const sentence = jsPsych.timelineVariable('sentence');
-      const left  = document.querySelector('.key-col.left');
-      const right = document.querySelector('.key-col.right');
-      let responded = false;
-
-      jsPsych.pluginAPI.getKeyboardResponse({
-        callback_function: function (info) {
-          if (responded) return;
-          responded = true;
-
-          const key = (info.key || '').toLowerCase();
-          if (key === 'f' && left)  left.classList.add('highlight');
-          if (key === 'j' && right) right.classList.add('highlight');
-
-          // after 1200ms, end the trial and save the response
-          setTimeout(function () {
-            jsPsych.finishTrial({
-              stimulus: sentence,
-              response: key,
-              response_meaning: key === 'j' ? 'True' : 'False',
-              rt: info.rt
-            });
-          }, 1200); // adjust: 1000–2000ms
-        },
-        valid_responses: ['f', 'j'],
-        persist: false
-      });
+    choices: ['f', 'j'],
+    response_ends_trial: true,
+    data: { stimulus: jsPsych.timelineVariable('sentence') },
+    on_finish: function (data) {
+      data.response_meaning = data.response === 'j' ? 'True' : 'False';
     }
   }],
   timeline_variables: politicalCharacterizations.map(sentence => ({ sentence })),
   randomize_order: false
 };
 
-// Build & run
+// Build and run experiment
 var experiment = [];
 experiment.push(
   coolInstructions,
